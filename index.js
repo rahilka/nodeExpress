df9c96e473f54432c4510e3243281cb324e42504
Rahilka Simonova <rahilka.simonova@mi-c3.com>
2017-02-19 19:51:33 +0100

    Added jade template engine

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ var port = process.env.PORT || 3000;
 
 app.use('/assets', express.static(__dirname + '/public'));
 
+app.set('view engine', 'ejs');
+
 app.use('/', function(req, res, next) {
   console.log('Request url: ' + req.url);
   next();
 })
 
 app.get('/', function(req, res) {
-  res.send('<html><head><link href=assets/style.css type=text/css rel=stylesheet /></head><body><h1>Hello World with express!!!</h1></body></html>');
+  res.render('index', { ID: req.params.id });
 });
 
 app.get('/api', function(req, res) {
@@ -19,8 +21,7 @@ app.get('/api', function(req, res) {
 });
 
 app.get('/person/:page/:id', function(req, res) {
-  res.send('<html><head></head><body><h1>Person: ' +
-            req.params.id + ' ' + req.params.page + '</h1></body></html>');
+  res.render('person', { ID: req.params.id })
 });
 
 app.listen(port);
