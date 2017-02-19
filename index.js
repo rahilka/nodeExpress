e209f6a3ea583c7a43a01313a79c8e72f02b80d2
Rahilka Simonova <rahilka.simonova@mi-c3.com>
2017-02-19 17:45:42 +0100

    Added route

--- index.js
+++ index.js
@@ -4,11 +4,16 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res) {
-  res.send('<html><head></head><body><h1>Hello World with express!!!</h1></body></html>')
+  res.send('<html><head></head><body><h1>Hello World with express!!!</h1></body></html>');
 });
 
 app.get('/api', function(req, res) {
   res.json({ firstname: 'Rahilka', lastname: 'Simonova' });
-})
+});
+
+app.get('/person/:page/:id', function(req, res) {
+  res.send('<html><head></head><body><h1>Person: ' +
+            req.params.id + ' ' + req.params.page + '</h1></body></html>');
+});
 
 app.listen(port);
