8ad53bee7b47b98d4cf62e040bc593700de42a05
Rahilka Simonova <rahilka.simonova@mi-c3.com>
2017-02-19 19:07:05 +0100

    Added static file

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
+app.use('/assets', express.static(__dirname + '/public'));
+
+app.use('/', function(req, res, next) {
+  console.log('Request url: ' + req.url);
+  next();
+})
+
 app.get('/', function(req, res) {
-  res.send('<html><head></head><body><h1>Hello World with express!!!</h1></body></html>');
+  res.send('<html><head><link href=assets/style.css type=text/css rel=stylesheet /></head><body><h1>Hello World with express!!!</h1></body></html>');
 });
 
 app.get('/api', function(req, res) {
